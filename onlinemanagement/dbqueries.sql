6305e9d16111870a7a3e2e2ecfb26bd40b14a9b7
Hemanth Chalumuri <hemanthunique@gmail.com>
2017-08-17 00:38:13 -0500

    Enhanced SPRING SECURITY by using JDBC User Service which picks credentials and roles from Database .

--- onlinemanagement/dbqueries.sql
+++ onlinemanagement/dbqueries.sql
@@ -5,3 +5,14 @@ CREATE TABLE product (
 	PRIMARY KEY ( product_id )
 
 );
+
+CREATE TABLE user_detail (
+	id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(50),
+	role VARCHAR(50),
+	password VARCHAR(60),
+	PRIMARY KEY (id )
+);
+
+INSERT INTO user_detail (username,role,password) VALUES ('admin','ADMIN','password');
+INSERT INTO user_detail (username,role,password) VALUES ('hemanth','USER','password');
